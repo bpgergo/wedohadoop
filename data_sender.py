5864c55998890efcc8e4fd80ed48bfa415956670
Janos Gub <jgub@hortonworks.com>
2016-12-10 03:45:22 +0100

    data sender with join

--- data_sender.py
+++ data_sender.py
@@ -2,24 +2,29 @@ import pandas as pd
 import time
 import socket
 import random
-from kafka import KafkaConsumer, KafkaProducer
-import threading
 
-class Producer(threading.Thread):
-    daemon = True
-    crm_weekly = pd.read_csv("~/wedohadoop/data/msc_weekly.csv", delimiter=";")
+crm_weekly = pd.read_csv("~/wedohadoop/data/crm_weekly.csv", delimiter=";")
+del crm_weekly["dataset"]
+msc_weekly = pd.read_csv("~/wedohadoop/data/msc_weekly.csv", delimiter=";")
+print(crm_weekly.head())
+print(msc_weekly.head())
+xx = msc_weekly.join(crm_weekly, lsuffix="azanyad", how="inner")
+xx.info()
+print(xx.tail())
+SLEEP = 0.0005;
 
-    def __init__(self, sleep_time):
-        self.sleep_time = sleep_time
 
-
-    def run(self, host="localhost", port=9092):
-        producer = KafkaProducer(bootstrap_servers=host+":"+str(port))
-
-        while True:
-            record = random.randint(0, self.crm_weekly.shape[0] - 1)
-            producer.send('data', bytes(";".join(str(x) for x in self.crm_weekly.ix[record])+"\n", "UTF-8"))
-            time.sleep(self.sleep_time)
-
-x = Producer(1)
-x.run()
+serversocket = socket.socket()
+# bind the socket to a public host, and a well-known port
+print("listening to connections")
+serversocket.bind(("localhost", 9999))
+serversocket.listen(5)
+while True:
+    # accept connections from outside
+    (clientsocket, address) = serversocket.accept()
+    print("Sending data in every ", SLEEP, "s")
+    while True:
+        time.sleep(SLEEP)
+        record = random.randint(0,crm_weekly.shape[0]-1)
+        #print(";".join(str(x) for x in crm_weekly.ix[record]))
+        clientsocket.send(bytes(";".join(str(x) for x in xx.ix[record])+"\n", 'UTF-8'))
