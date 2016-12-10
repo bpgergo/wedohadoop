2f7eb0dd4a601f47e833765ed5ca42d3d06a4397
Janos Gub <jgub@hortonworks.com>
2016-12-10 01:33:34 +0100

    kafkazing data_sender

--- data_sender.py
+++ data_sender.py
@@ -2,22 +2,24 @@ import pandas as pd
 import time
 import socket
 import random
+from kafka import KafkaConsumer, KafkaProducer
+import threading
 
-crm_weekly = pd.read_csv("~/wedohadoop/data/msc_weekly.csv", delimiter=";")
-SLEEP = 0.0001;
+class Producer(threading.Thread):
+    daemon = True
+    crm_weekly = pd.read_csv("~/wedohadoop/data/msc_weekly.csv", delimiter=";")
+
+    def __init__(self, sleep_time):
+        self.sleep_time = sleep_time
 
 
-serversocket = socket.socket()
-# bind the socket to a public host, and a well-known port
-print("listening to connections")
-serversocket.bind(("localhost", 9999))
-serversocket.listen(5)
-while True:
-    # accept connections from outside
-    (clientsocket, address) = serversocket.accept()
-    print("Sending data in every ", SLEEP, "s")
-    while True:
-        time.sleep(SLEEP)
-        record = random.randint(0,crm_weekly.shape[0]-1)
-        print(";".join(str(x) for x in crm_weekly.ix[record]))
-        clientsocket.send(bytes(";".join(str(x) for x in crm_weekly.ix[record])+"\n", 'UTF-8'))
+    def run(self, host="localhost", port=9092):
+        producer = KafkaProducer(bootstrap_servers=host+":"+str(port))
+
+        while True:
+            record = random.randint(0, self.crm_weekly.shape[0] - 1)
+            producer.send('data', bytes(";".join(str(x) for x in self.crm_weekly.ix[record])+"\n", "UTF-8"))
+            time.sleep(self.sleep_time)
+
+x = Producer(1)
+x.run()
